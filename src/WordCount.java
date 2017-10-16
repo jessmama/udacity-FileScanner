fdfc5ae1cb7af1aacfd6aa531f15b15f82c442f2
JessicaLee <chiahui.lee@fubon.com>
2017-10-16 16:00:15 +0800

    Finish WordsCount.

--- src/WordCount.java
+++ src/WordCount.java
@@ -5,13 +5,17 @@ import java.util.Scanner;
 public class WordCount {
     public static void main(String[] args) throws FileNotFoundException {
         String line="";
+        int wordsCount=0;
+
         File file =new File("TwoCities.txt");
         Scanner filescanner=new Scanner(file);
 
         while(filescanner.hasNextLine()){
             line=filescanner.nextLine();
+            String[] words = line.split(" ");
+            wordsCount=wordsCount+words.length;
         }
-        String[] word = line.split("");
-
+        System.out.println("Total words count is :" + wordsCount);
+       
     }
 }
